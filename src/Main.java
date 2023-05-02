c7e8b55b6a254ec6a281238bf724773fead03c00
ambrishba-sacumen <ambrish.ba@sacumen.com>
2023-05-02 11:19:21 +0530

    deleted

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("java script");
+
         for(int i=0;i<5;i++)
         {  if(i!=0)
             System.out.println(i);
         }
-        System.out.println("java");
+        
     }
 }
